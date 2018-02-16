7c75976015649c286e1ff0d3abf90af216c4b036
David Clark <primecomp@Davids-MacBook-Pro.local>
2018-02-16 11:51:26 -0600

    added title animation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -22,6 +22,15 @@ myApp.controller('HomeController', function() {
     console.log('Home Controller was Loaded');
     var self = this;
     self.homeMessage = 'Home Page Message';
+    var msg = new SpeechSynthesisUtterance('Dave Clark');
+        var voices = window.speechSynthesis.getVoices();
+        msg.voice = voices[25]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
+        msg.voiceURI = 'native';
+        msg.volume = 2; // 0 to 1
+        msg.rate = .8; // 0.1 to 10
+        msg.pitch = 1; //0 to 2       
+        msg.lang = 'en-US';
+        window.speechSynthesis.speak(msg);
 });
 
 myApp.controller('BiographyController', function() {
